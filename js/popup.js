0e952b6e6454d62f6a89cdc562cded4d1244b05f
enzo masson <enzo_2510@hotmail.fr>
2021-03-20 14:12:17 +0100

    Update popup.js
    passer la couleur du joueur dans la fonction communicante

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){ 
     $('#best_move_button').click(function(){
+      console.log($('#color :selected').val())
       chrome.tabs.query({active: true, currentWindow: true},function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {message: "bestMove"});
       });
